34be9be1e3ca8f6cb8faa0c749b93d8ebb58affa
Rich Abdill <richard.abdill@gmail.com>
2020-10-22 13:39:18 -0400

    2020 polling

--- states.js
+++ states.js
@@ -1,129 +1,130 @@
+// https://www.270towin.com/
 states = [
   {
     "name": "Alabama",
     "electors" : 9,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Alaska",
     "electors" : 3,
     "category": "probably red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Arizona",
     "electors": 11,
-    "category": "probably red",
+    "category": "lean blue",
     "winner": ""
   },
   {
     "name": "Arkansas",
     "electors": 6,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "California",
     "electors": 55,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "Colorado",
     "electors": 9,
     "category": "probably blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "Connecticut",
     "electors": 7,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "DC",
     "electors": 3,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "Delaware",
     "electors": 3,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "<strong>Florida</strong>",
     "electors": 29,
     "category": "toss-up",
-    "winner": "trump"
+    "winner": ""
   },
   {
-    "name": "Georgia",
+    "name": "<strong>Georgia</strong>",
     "electors": 16,
-    "category": "strong red",
-    "winner": "trump"
+    "category": "toss-up",
+    "winner": ""
   },
   {
     "name": "Hawaii",
     "electors": 4,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "Idaho",
     "electors": 4,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Illinois",
     "electors": 20,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "Indiana",
     "electors": 11,
-    "category": "strong red",
-    "winner": "trump"
+    "category": "probably red",
+    "winner": ""
   },
   {
     "name": "Iowa",
     "electors": 6,
-    "category": "probably red",
-    "winner": "trump"
+    "category": "toss-up",
+    "winner": ""
   },
   {
     "name": "Kansas",
     "electors": 6,
-    "category": "strong red",
-    "winner": "trump"
+    "category": "probably red",
+    "winner": ""
   },
   {
     "name": "Kentucky",
     "electors": 8,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Louisiana",
     "electors": 8,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Maine statewide",
     "electors": 2,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "Maine District 1",
     "electors": 1,
-    "category": "strong blue",
-    "winner": "clinton"
+    "category": "probably blue",
+    "winner": ""
   },
   {
     "name": "<strong>Maine District 2</strong>",
@@ -135,73 +136,73 @@ states = [
     "name": "Maryland",
     "electors": 10,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "Massachusetts",
     "electors": 11,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
-    "name": "<strong>Michigan</strong>",
+    "name": "Michigan",
     "electors": 16,
-    "category": "toss-up",
+    "category": "probably blue",
     "winner": ""
   },
   {
     "name": "Minnesota",
     "electors": 10,
-    "category": "strong blue",
+    "category": "lean blue",
     "winner": ""
   },
   {
     "name": "Mississippi",
     "electors": 6,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Missouri",
     "electors": 10,
-    "category": "strong red",
-    "winner": "trump"
+    "category": "probably red",
+    "winner": ""
   },
   {
     "name": "Montana",
     "electors": 3,
-    "category": "strong red",
-    "winner": "trump"
+    "category": "probably red",
+    "winner": ""
   },
   {
     "name": "Nebraska statewide",
     "electors": 2,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Nebraska District 1",
     "electors": 1,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Nebraska District 2",
     "electors": 1,
-    "category": "probably red",
+    "category": "lean blue",
     "winner": ""
   },
   {
     "name": "Nebraska District 3",
     "electors": 1,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "<strong>Nevada</strong>",
     "electors": 6,
-    "category": "toss-up",
-    "winner": "clinton"
+    "category": "lean blue",
+    "winner": ""
   },
   {
     "name": "New Hampshire",
@@ -213,126 +214,126 @@ states = [
     "name": "New Jersey",
     "electors": 14,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "New Mexico",
     "electors": 5,
-    "category": "strong blue",
-    "winner": "clinton"
+    "category": "probably blue",
+    "winner": ""
   },
   {
     "name": "New York",
     "electors": 29,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "<strong>North Carolina</strong>",
     "electors": 15,
     "category": "toss-up",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "North Dakota",
     "electors": 3,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Ohio",
     "electors": 18,
-    "category": "probably red",
-    "winner": "trump"
+    "category": "toss-up",
+    "winner": ""
   },
   {
     "name": "Oklahoma",
     "electors": 7,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Oregon",
     "electors": 7,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "Pennsylvania",
     "electors": 20,
-    "category": "probably blue",
+    "category": "lean blue",
     "winner": ""
   },
   {
     "name": "Rhode Island",
     "electors": 4,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "South Carolina",
     "electors": 9,
-    "category": "strong red",
-    "winner": "trump"
+    "category": "probably red",
+    "winner": ""
   },
   {
     "name": "South Dakota",
     "electors": 3,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Tennessee",
     "electors": 11,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Texas",
     "electors": 38,
-    "category": "strong red",
-    "winner": "trump"
+    "category": "lean red",
+    "winner": ""
   },
   {
     "name": "Utah",
     "electors": 6,
     "category": "probably red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "Vermont",
     "electors": 3,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "Virginia",
     "electors": 13,
-    "category": "strong blue",
-    "winner": "clinton"
+    "category": "probably blue",
+    "winner": ""
   },
   {
     "name": "Washington",
     "electors": 12,
     "category": "strong blue",
-    "winner": "clinton"
+    "winner": ""
   },
   {
     "name": "West Virginia",
     "electors": 5,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   },
   {
     "name": "<strong>Wisconsin</strong>",
     "electors": 10,
-    "category": "toss-up",
+    "category": "lean blue",
     "winner": ""
   },
   {
     "name": "Wyoming",
     "electors": 3,
     "category": "strong red",
-    "winner": "trump"
+    "winner": ""
   }
 ];
